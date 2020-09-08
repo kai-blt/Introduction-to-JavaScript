565dcff75c628951c5d40ea073886f3ba16f6311
kai-blt <blaitong@gmail.com>
2020-09-08 08:36:45 -0700

    completed task 6

--- index.js
+++ index.js
@@ -161,7 +161,14 @@ console.log('100 ft to cm is: ' + feetToCM(100));
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
+function annoyingSong(number)   {
+    for (i = number; i > 0; i--) {
+        console.log(i + ' bottles of soda on the wall, ' + i + ' bottles of soda, take one down pass it around ' + (i - 1) + ' bottles of soda on the wall.')
+        
+    }
+}
+
+annoyingSong(10);
 
 
 
